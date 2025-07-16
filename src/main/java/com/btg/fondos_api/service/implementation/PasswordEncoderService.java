d5b14e02fa61571967037658ad26b0d01cc3f4f9
Jorge Mario <j-mario9715@hotmail.com>
2025-07-15 19:12:46 -0500

    ajustes deploy

--- src/main/java/com/btg/fondos_api/service/implementation/PasswordEncoderService.java
+++ src/main/java/com/btg/fondos_api/service/implementation/PasswordEncoderService.java
@@ -2,14 +2,14 @@ package com.btg.fondos_api.service.implementation;
 
 import com.btg.fondos_api.service.IPasswordEncoderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PasswordEncoderService implements IPasswordEncoderService {
 
-    private final BCryptPasswordEncoder encoder;
+    private final PasswordEncoder encoder;
 
     @Override
     public String encode(String rawPassword) {
